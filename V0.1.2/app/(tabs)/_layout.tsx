be367043a4266fd60df99f91ee82f5ca88178078
Tuur007 <tuurdebruyne@hotmail.com>
2026-05-05 00:54:38 +0100

    Update _layout.tsx

--- V0.1.2/app/(tabs)/_layout.tsx
+++ V0.1.2/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { Colors } from '../../components/ui/colors';
+import { colors } from '../../constants/Designsystem';
 
 export default function TabLayout() {
   return (

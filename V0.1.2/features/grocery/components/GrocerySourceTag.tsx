b49b3c4434b89d33d0765025b06c26c9f21c33e6
Tuur007 <tuurdebruyne@hotmail.com>
2026-05-05 00:48:18 +0100

    Update GrocerySourceTag.tsx

--- V0.1.2/features/grocery/components/GrocerySourceTag.tsx
+++ V0.1.2/features/grocery/components/GrocerySourceTag.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { Colors } from '../../../components/ui/colors';
+import { colors } from '../../../constants/Designsystem';
 import { SourceLineage } from '../../../types/grocery';
 
 interface GrocerySourceTagProps {
